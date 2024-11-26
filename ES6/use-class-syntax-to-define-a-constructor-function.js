42ed6b4980d538822ec06df99fe360d8298479d8
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-11-26 17:41:59 +0530

    Add files via upload

---
+++ ES6/use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name; // Assign the name property to the instance
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'

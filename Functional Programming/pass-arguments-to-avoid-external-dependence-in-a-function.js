087c405b48d7f193e93bc36d667bc35e48f861ca
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-11-29 10:47:38 +0530

    Create pass-arguments-to-avoid-external-dependence-in-a-function.js

---
+++ Functional Programming/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,12 @@
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer(value) {
+  return value + 1;
+
+  // Add your code above this line
+}
+
+var differentValue = incrementer(fixedValue); 
+console.log(fixedValue); 
